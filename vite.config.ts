5856124da3db378e7cdcfd449aecfa2e6ca2130f
Daniel Jonasson <1116766+djonasson@users.noreply.github.com>
2026-02-03 18:31:12 +0100

    test

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => {
-  const base = command === 'build' ? '/hemoio/' : '/';
+  const base = command === 'build' ? '/HemoIO/' : '/';
 
   return {
     base,
